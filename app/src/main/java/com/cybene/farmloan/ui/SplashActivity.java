68ea00ade1949d35feff849256f5fc4087aeec87
TheOnlySmartBoy <o2jose43@gmail.com>
2020-09-19 12:19:59 +0300

    Added an sPLASHSCREEN activity

--- app/src/main/java/com/cybene/farmloan/ui/SplashActivity.java
+++ app/src/main/java/com/cybene/farmloan/ui/SplashActivity.java
@@ -2,7 +2,11 @@ package com.cybene.farmloan.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.cybene.farmloan.R;
 
@@ -11,6 +15,26 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // make the activity on full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        // hide the action bar
+        getSupportActionBar().hide();
+        splash();
+    }
+    private void splash() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //User has logged in
+                //Show the HomeActivity
+                Intent home = new Intent(SplashActivity.this, SignInActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },5000);
+
     }
 }
